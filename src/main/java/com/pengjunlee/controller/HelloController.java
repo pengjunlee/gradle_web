2045314915685e844b9e8ccef3400e4cc3f2b224
pengjunlee <pjli@winnermedical.com>
2021-04-11 17:20:34 +0800

    init

---
+++ src/main/java/com/pengjunlee/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.pengjunlee.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+
+    @RequestMapping("/list")
+    public String toList(){
+        return "list";
+    }
+}
